a61af60c04ef19af419eed36cfcff6c355b21319
unknown <harshitatriredmi@gmail.com>
2020-07-08 23:25:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.get("/",function(req,res){
+res.sendfile('index.html');
+});
+app.listen(3000 ,()=>{
+    console.log("server is running")
+});
