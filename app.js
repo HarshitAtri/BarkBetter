d9184e1ea897a178586cd67952cf3f888e7549f9
unknown <harshitatriredmi@gmail.com>
2020-07-08 23:52:43 +0530

    Changed

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use('/', express.static(__dirname + '/public'));
 app.get("/",function(req,res){
 res.sendfile('index.html');
 });
-app.listen(process.env.PORT||3000 ,()=>{
+app.listen(process.env.PORT || 3000 ,()=>{
     console.log("server is running")
 });
