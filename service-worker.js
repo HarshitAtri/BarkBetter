3f25f00c9019ff522a9015cf5f99f78b51167f1c
unknown <harshitatriredmi@gmail.com>
2020-07-09 12:18:12 +0530

    6th attempt

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,11 @@
-// let deferredPrompt;
-// window.addEventListener('beforeinstallprompt', (e) => {
-// // Prevent Chrome 67 and earlier from automatically showing the prompt
-// e.preventDefault();
-// // Stash the event so it can be triggered later.
-// deferredPrompt = e;
-// // Update UI notify the user they can add to home screen
-// document.getElementById("pwaa").style.display = 'block';
-// });
-// document.getElementById("pwaa").addEventListener('click', (e) => {
-// // hide our user interface that shows our A2HS button
-// document.getElementById("pwaa").style.display = 'none';
-// // Show the prompt
-// deferredPrompt.prompt();
-// // Wait for the user to respond to the prompt
-// deferredPrompt.userChoice
-// .then((choiceResult) => {
-//   if (choiceResult.outcome === 'accepted') {
-//     console.log('User accepted the A2HS prompt');
-//   } else {
-//     console.log('User dismissed the A2HS prompt');
-//   }
-//   deferredPrompt = null;
-// });
-// });
+console.log('Started', self);
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+});
