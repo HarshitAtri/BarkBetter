769e4aa71e8e1097c93e5303709c8347f4dc690f
unknown <harshitatriredmi@gmail.com>
2020-07-09 12:38:30 +0530

    6th attempt

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express()
 
 app.use('/', express.static(__dirname + '/public'));
 
+
 app.get("/",function(req,res){
-res.sendfile('index.html');
-});
+    res.sendfile('index.html');
+    });
+app.get("/service-worker.js",function(req,res){
+        
+        });
 app.listen(process.env.PORT || 3000 ,()=>{
     console.log("server is running")
 });
